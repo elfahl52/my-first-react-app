43156ef57638233f0072cbdbb2683e0902b5dfa3
Khaled Elfahl <khaledelfahl56@gmail.com>
2026-05-25 00:21:56 +0300

    Project Updated

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './Contact.css'
-// 1. استيراد الأيقونات من المكتبات المختلفة
 import { FaUser, FaEnvelope, FaPhone, FaMapMarkerAlt, FaPaperPlane } from 'react-icons/fa';
-import { FiSend } from 'react-icons/fi'; // أيقونة إرسال أنيقة
+import { FiSend } from 'react-icons/fi'; 
 
 function Contact() {
   return (
@@ -11,35 +10,35 @@ function Contact() {
         <h2>Contact Us</h2>
         <div className="container">
           
-          {/* قسم الفورم */}
+        
           <div className="left">
             <form action="#">
               
-              {/* حقل الاسم مع أيقونة */}
+              
               <div className="input-group">
                 <FaUser className="input-icon" />
                 <input type="text" placeholder="Your Name" />
               </div>
 
-              {/* حقل الإيميل مع أيقونة */}
+             
               <div className="input-group">
                 <FaEnvelope className="input-icon" />
                 <input type="email" placeholder="Your Email" />
               </div>
 
-              {/* حقل التليفون مع أيقونة */}
+         
               <div className="input-group">
                 <FaPhone className="input-icon" />
                 <input type="number" placeholder="Your Phone" />
               </div>
 
-              {/* حقل الرسالة */}
+          
               <div className="input-group">
                 <FaPaperPlane className="input-icon" style={{marginTop: '12px'}} />
                 <textarea placeholder="Your Message"></textarea>
               </div>
 
-              {/* زرار الإرسال */}
+               
               <button type="submit" className="submit-btn">
                 Send Message <FiSend style={{marginLeft: '8px'}} />
               </button>
@@ -47,7 +46,7 @@ function Contact() {
             </form>
           </div>
 
-          {/* قسم معلومات التواصل */}
+          
           <div className="right">
             <h3>Contact Information</h3>
             
